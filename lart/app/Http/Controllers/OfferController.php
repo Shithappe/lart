f99dbcd1b31901bbbd1fe4e26d677a4f94ff408c
Shithappe <greenmad.b50@gmail.com>
2022-12-22 02:39:14 +0200

    init

---
+++ lart/app/Http/Controllers/OfferController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use App\Http\Requests\StoreOfferRequest;
+use App\Http\Requests\UpdateOfferRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class OfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Offer::get();
+        // return DB::select('select * from offers');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|string'
+        ]);
+
+        
+        $offer = [
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price')
+        ];
+        
+        return Offer::create($offer);
+    }
+
+    public function getByCategory_id(Request $request)
+    {
+        $id = $request->input('id');
+        return DB::select('select * from offers where category_id = :id', ['id' => $id]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOfferRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOfferRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOfferRequest  $request
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOfferRequest $request, Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Offer $offer)
+    {
+        //
+    }
+}
---
+++ lart/app/Models/Offer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'category_id'
+    ];
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+}
